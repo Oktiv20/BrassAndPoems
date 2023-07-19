9811a4c8ddf6fc3b8a6f2b388a0a8e5e3e37bc7a
Kyle Blunt <kbluntaudio@gmail.com>
2023-07-18 20:38:16 -0500

    Implemented Main menu and  display products

--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -5,25 +5,53 @@ using System.Threading.Channels;
 
 List<Product> products = new List<Product>()
 {
-    new Product { Name = "Tuba", Price = 3000.00m, ProductTypeId = 1},
-    new Product { Name = "Saxophone", Price = 10000.00m, ProductTypeId = 1},
-    new Product { Name = "Trumpet", Price = 50000.00m, ProductTypeId = 1},
-    new Product { Name = "Come In", Price = 1000.00m, ProductTypeId = 2},
-    new Product { Name = "Music Swims Back To Me", Price = 5000.00m, ProductTypeId = 2},
+    new Product() 
+    { 
+        Name = "Tuba", 
+        Price = 3000.00m, 
+        ProductTypeId = 1
+    },
+    new Product() 
+    { 
+        Name = "Saxophone", 
+        Price = 10000.00m, 
+        ProductTypeId = 1
+    },
+    new Product() 
+    { 
+        Name = "Trumpet", 
+        Price = 50000.00m, 
+        ProductTypeId = 1
+    },
+    new Product() 
+    { 
+        Name = "Come In", 
+        Price = 1000.00m, 
+        ProductTypeId = 2
+    },
+    new Product() 
+    { 
+        Name = "Music Swims Back To Me", 
+        Price = 5000.00m, 
+        ProductTypeId = 2
+    },
 };
 
 //create a "productTypes" variable here with a List of ProductTypes, and add "Brass" and "Poem" types to the List. 
 
 List<ProductType> productTypes = new List<ProductType>()
 {
-    new ProductType { Title = "Brass", Id = 1 },
-    new ProductType { Title = "Poems", Id = 2 },
+    new ProductType() 
+    { 
+        Title = "Brass", 
+        Id = 1 
+    },
+    new ProductType() 
+    { 
+        Title = "Poems", 
+        Id = 2 
+    },
 };
-//put your greeting here
-
-string greeting = "Welcome to Brass and Poems!";
-Console.WriteLine(greeting);
-string? choice = null;
 
 
 // MAIN MENU
@@ -38,24 +66,96 @@ void App()
         DisplayMenu();
         string input = Console.ReadLine();
 
-        
+        if (int.TryParse(input, out choice))
+        {
+            switch (choice)
+            {
+                case 1:
+                    DisplayAllProducts(products, productTypes);
+                    break;
+                case 2:
+                    DeleteProduct(products, productTypes);
+                    break;
+                case 3:
+                    // AddProduct();
+                    break;
+                case 4:
+                    // UpdateProduct();
+                    break;
+                case 5:
+                    Console.WriteLine("Exit App");
+                    return;
+                default:
+                    Console.WriteLine("Please try again");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please try again");
+        };
     }
+    while (choice != 5);
 }
 
 void DisplayMenu()
 {
-    throw new NotImplementedException();
+    Console.WriteLine(@"Please choose an option below:
+        1. DisplayAllProducts
+        2. DeleteProduct
+        3. AddProduct
+        4. UpdateProduct
+        5. Exit"); 
 }
 
 void DisplayAllProducts(List<Product> products, List<ProductType> productTypes)
 {
-    throw new NotImplementedException();
+    for (int i = 0; i < products.Count; i++)
+    {
+        Product product = products[i];
+        ProductType productType = productTypes.FirstOrDefault(p => p.Id == product.ProductTypeId);
+        Console.WriteLine($"{i + 1}. Title: {productType.Title} \t Price: {product.Price:C} \t Product Name: {product.Name}");
+    };
 }
 
-void DeleteProduct(List<Product> products, List<ProductType> productTypes)
+/*void DeleteProduct(List<Product> products, List<ProductType> productTypes)
 {
-    throw new NotImplementedException();
-}
+    Console.WriteLine("Delete a product:");
+
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name}");
+    }
+
+    Product DeleteProduct = null!;
+
+    while (DeleteProduct == null)
+    {
+        try
+        {
+            Console.WriteLine("Enter the product number to remove:");
+            int response = int.Parse(Console.ReadLine()!.Trim());
+            DeleteProduct = products[response - 1];
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please enter a valid number from the list.");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please enter a valid number from list.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Console.WriteLine("Not a valid choice. Please try again.");
+        }
+    }
+
+    products.Remove(DeleteProduct);
+
+    Console.WriteLine($"You've removed {DeleteProduct.Name}");
+}*/
 
 void AddProduct(List<Product> products, List<ProductType> productTypes)
 {
@@ -66,6 +166,8 @@ void UpdateProduct(List<Product> products, List<ProductType> productTypes)
 {
     throw new NotImplementedException();
 }
+
+App();
 
 // don't move or change this!
 public partial class Program { }
